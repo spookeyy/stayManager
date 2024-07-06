226efe611fe34efc562f7e2297e9018c846e1a55
spookeyy <pangasmeshack@gmail.com>
2024-07-07 02:45:43 +0300

    user model created

---
+++ backend/server/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+# from flask_bcrypt import Bcrypt # Flask-Bcrypt for password hashing
+# from flask_mail import Mail # Flask-Mail for sending emails
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+cors = CORS(app)
+# bcrypt = Bcrypt(app)
+# mail = Mail(app)
+
+from routes import auth, user, room, booking, review
+
+
+app.register_blueprint(auth.bp)
+app.register_blueprint(user.bp)
+app.register_blueprint(room.bp)
+app.register_blueprint(booking.bp)
+app.register_blueprint(review.bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/server/models/user.py
@@ -0,0 +1,23 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    username = db.column(db.String(80), unique=True, nullable=False)
+    email = db.column(db.string(120), unique=True, nullable=False)
+    password = db.column(db.String(120), nullable=False)
+    phone_number = db.column(db.String(120), unique=True, nullable=False)
+    profile_photo = db.column(db.String(120), nullable=True)
+    is_admin = db.column(db.Boolean, nullable=False, default=False)
+
+    bookings = db.relationship('Booking', backref='user', lazy='dynamic')
+    reviews = db.relationship('Review', backref='user', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
