0e35190f12f0b9a66ee46bc2ea32dcdd39b0374b
spookeyy <pangasmeshack@gmail.com>
2024-07-07 17:44:39 +0300

    app

--- backend/server/app.py
+++ backend/server/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flask_migrate import Migrate
 # from flask_bcrypt import Bcrypt # Flask-Bcrypt for password hashing
 # from flask_mail import Mail # Flask-Mail for sending emails
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 jwt = JWTManager(app)
 cors = CORS(app)
 
