7bce4f15996443380cb8cf03cb2351a9e4bd7961
spookeyy <pangasmeshack@gmail.com>
2024-07-07 12:34:54 +0300

    init for models

---
+++ backend/server/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .room import Room
+from .review import Review
+from .booking import Booking
+
+# init file imports all model classes(models)
+# making it easier to import them in other files in the project
