a7288630522b2ea70c3044d186a1e320a2edcdb4
spookeyy <pangasmeshack@gmail.com>
2024-07-10 22:54:08 +0300

    authcontext update

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import useHistory from "react-router-dom/cjs/react-router-dom.min";
+import {useHistory} from "react-router-dom";
 
 export const UserContext = createContext();
 
