ccb55697af9f24eb3cb910c6b21afaeb30b67422
spookeyy <pangasmeshack@gmail.com>
2024-07-11 11:48:05 +0300

    admin bookins

--- frontend/src/pages/AdminBookings.jsx
+++ frontend/src/pages/AdminBookings.jsx
@@ -1,6 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function AdminBookings({ bookings, onCancelBooking }) {
+export default function AdminBookings() {
+     const [bookings, setBookings] = useState([]);
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+      fetchBookings();
+      fetchRooms();
+    //   fetchHotels();
+    }, []);
+
+     const fetchBookings = async () => {
+       try {
+         const response = await fetch("http://localhost:5000/bookings", {
+           headers: {
+             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+           },
+         });
+         if (response.ok) {
+           const data = await response.json();
+           setBookings(data);
+         } else {
+           throw new Error("Failed to fetch bookings");
+         }
+       } catch (error) {
+         console.error("Error fetching bookings:", error);
+         toast.error("Failed to fetch bookings");
+       }
+     };
+
+     const fetchRooms = async () => {
+       try {
+         const response = await fetch("http://localhost:5000/rooms", {
+           headers: {
+             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+           },
+         });
+         if (response.ok) {
+           const data = await response.json();
+           setRooms(data);
+         } else {
+           throw new Error("Failed to fetch rooms");
+         }
+       } catch (error) {
+         console.error("Error fetching rooms:", error);
+         toast.error("Failed to fetch rooms");
+       }
+     };
+
+    const handleCancelBooking = async (bookingId) => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/bookings/${bookingId}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            },
+          }
+        );
+        if (response.ok) {
+          toast.success("Booking cancelled successfully");
+          fetchBookings();
+          fetchRooms();
+        } else {
+          throw new Error("Failed to cancel booking");
+        }
+      } catch (error) {
+        console.error("Error cancelling booking:", error);
+        toast.error("Failed to cancel booking");
+      }
+    };
+
   return (
     <div className="bg-gray-700 rounded-lg shadow-md p-6 mt-8 mb-8">
       <h3 className="text-2xl font-semibold mb-4 text-gray-200">Bookings</h3>
