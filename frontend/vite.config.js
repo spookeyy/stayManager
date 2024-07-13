ccefddd4cb6a7effa40a96293f4c0ad63111b084
spookeyy <pangasmeshack@gmail.com>
2024-07-13 17:21:08 +0300

    netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
+  publicDir: 'public', // This ensures files in the public directory are copied to dist
 })
